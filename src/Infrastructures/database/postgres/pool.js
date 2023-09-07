e0bfa3a9164e8c33311447fee65780b017abe7ce
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2023-09-07 20:46:01 +0700

    create connection pool

---
+++ src/Infrastructures/database/postgres/pool.js
@@ -0,0 +1,17 @@
+/* istanbul ignore file */
+// untuk memberi tahu jest bahwa berkas ini tidak perlu diuji
+// dan tidak dimasukkan ke dalam coverage test
+
+const { Pool } = require('pg');
+
+const testConfig = {
+  host: process.env.PGHOST_TEST,
+  port: process.env.PGPORT_TEST,
+  user: process.env.PGUSER_TEST,
+  password: process.env.PGPASSWORD_TEST,
+  database: process.env.PGDATABASE_TEST,
+};
+
+const pool = process.env.NODE_ENV === 'test' ? new Pool(testConfig) : new Pool();
+
+module.exports = pool;
